3b2d9782441f9226ad35187453024a209c0e59f1
seris370 <1812563232@qq.com>
2019-09-30 23:19:03 +0800

    init commit

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -23,11 +23,13 @@ public class Person {
     // Data fields
     private final Address address;
     private final Set<Tag> tags = new HashSet<>();
+    private final Remark remark;
 
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
+    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags, Remark remark) {
+        this.remark = remark;
         requireAllNonNull(name, phone, email, address, tags);
         this.name = name;
         this.phone = phone;
@@ -117,4 +119,7 @@ public class Person {
         return builder.toString();
     }
 
+    public Remark getRemark() {
+        return remark;
+    }
 }
